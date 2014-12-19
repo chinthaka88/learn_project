8d28a325fb8a9258a2b8d71abfe2ba1be962cf61
chinthaka88 <chinthaka88.sub@gmail.com>
2014-12-19 17:23:10 +0530

    commit-2014-12-19-5.22

--- Ecommerce/Program.cs
+++ Ecommerce/Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DALInterfaces;
+using DAL_EF;
+
+using ServiceInterfaces;
+using Services;
+using Entity;
+using DTO;
+
 namespace Ecommerce
 {
     class Program
     {
         static void Main(string[] args)
         {
+         IAppErrors errors=new AppErrors();
+            IItemService service = new ItemService(errors,new ItemRepository());
+            ItemDTO ex = new ItemDTO();
+            ex.Description = "Data By EF";
+            ex.CutOffMark = 24;
+            ex.CourseID = 1;
+            service.AddNewExam(ex);
+
+            if (errors.IsValid)
+             {
+                 Console.WriteLine("Success");
+             }
+            else
+             {
+                 foreach (Error er in errors.GetErrors())
+                 {
+                     Console.WriteLine(er.Value);
+                 }
+             }
+
+            IEnumerable<ExaminationDTO> exams = service.GetExams();
+             foreach(var x in exams)
+             {
+                 Console.WriteLine(x.CourseID + "|" + x.Description +"|" +x.CutOffMark);
+             }
+             Console.ReadLine();
+                             
         }
     }
 }
---
+++ Services/ItemService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceInterfaces;
+using Entity;
+using DALInterfaces;
+using DTO;
+
+namespace Services
+{
+    public class ItemService : IItemService
+    {
+         private IAppErrors modelerrors;
+        private IItemDataAccess  repository;
+
+        public ItemService(IAppErrors modelstate, IItemDataAccess repository)
+        {
+            this.modelerrors = modelstate;
+            this.repository = repository;
+        }
+
+        public bool AddNewExam(ItemDTO ex)
+        {
+            if (this.Validate(ex))
+            {
+                repository.AddNew(ex);
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateExam(ItemDTO ex)
+        {
+            if (this.Validate(ex))
+            {
+                repository.AddNew(ex);
+                return true;
+            }
+            return false;
+        }
+
+        public bool DeleteExam(int itemid)
+        {
+            repository.Delete(itemid);
+            return true;
+        }
+
+        public IEnumerable<ItemDTO> GetItems()
+        {
+            return repository.GetItems();
+        }
+
+        public bool Validate(ItemDTO item)
+        {
+            if (item.Description == null || item.Description == "")
+                modelerrors.AddError("ExamName", "Please Enter Exam Name");
+            if (item.Qty == 0)
+                modelerrors.AddError("PassMark", "Invalid Pass Mark");
+
+            return modelerrors.IsValid;
+        }
+    }
+}
